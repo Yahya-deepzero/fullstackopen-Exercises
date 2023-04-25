3d6239d98d784f5ef846211f8be409428105a7fe
Yahya-deepzero <131654962+Yahya-deepzero@users.noreply.github.com>
2023-04-25 15:45:45 +0400

    Create 1.3.js

---
+++ part1/1.3.js
@@ -0,0 +1,45 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  console.log(props.part[0].name, props.part[0].exercises);
+
+  return (
+    <div>
+      <Part name={props.part[0].name} exercises={props.part[0].exercises} />
+      <Part name={props.part[1].name} exercises={props.part[1].exercises} />
+      <Part name={props.part[2].name} exercises={props.part[2].exercises} />
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  );
+};
+
+const Total = (props) => {
+  return <p>Number of exercises {props.total}</p>;
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+
+  const part1 = { name: "Fundamentals of React", exercises: 10 };
+  const part2 = { name: "Using props to pass data", exercises: 7 };
+  const part3 = { name: "State of a component", exercises: 14 };
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={[part1, part2, part3]} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+    </div>
+  );
+};
+
+export default App;
